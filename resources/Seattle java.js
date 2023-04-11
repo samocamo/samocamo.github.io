35301b64bdc357d530a8461fbb4466fc71201a51
samocamo <121742387+samocamo@users.noreply.github.com>
2023-04-10 22:56:06 -0700

    Initial commit

---
+++ resources/Seattle java.js
@@ -0,0 +1,36 @@
+const hamburger = document.getElementById("hamburger")
+hamburger.addEventListener('click', function(e) {
+  const ul = document.querySelector('nav > ul');
+  ul.classList.toggle('menu-slide');
+  hamburger.classList.toggle('cross');
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction1() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function myFunction2() {
+  document.getElementById("myDropdown-2").classList.toggle("show");
+}
+function myFunction3() {
+  document.getElementById("myDropdown-3").classList.toggle("show");
+}
+function myFunction4() {
+  document.getElementById("myDropdown-4").classList.toggle("show");
+}
